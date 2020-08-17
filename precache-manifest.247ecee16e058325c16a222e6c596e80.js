6bab2ca1ba2b5879e8bd3a0607ecaab0d6546e93
yogev <byogev18185@gmail.com>
2020-08-17 22:47:05 +0300

    Updates

---
+++ precache-manifest.247ecee16e058325c16a222e6c596e80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4178202d3600003e41f72ed170acf259",
+    "url": "/yogev-betito-14-8-2020/index.html"
+  },
+  {
+    "revision": "123edc26417c750f7276",
+    "url": "/yogev-betito-14-8-2020/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7331289ddd5319cfba73",
+    "url": "/yogev-betito-14-8-2020/static/css/main.5fca92b1.chunk.css"
+  },
+  {
+    "revision": "123edc26417c750f7276",
+    "url": "/yogev-betito-14-8-2020/static/js/2.6b82cc0f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/yogev-betito-14-8-2020/static/js/2.6b82cc0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7331289ddd5319cfba73",
+    "url": "/yogev-betito-14-8-2020/static/js/main.dd0a70be.chunk.js"
+  },
+  {
+    "revision": "e2ddbc5866f11666a5a6",
+    "url": "/yogev-betito-14-8-2020/static/js/runtime-main.350128e9.js"
+  }
+]);
